12081cb5a44cddb516ff92dcdcd40a78e7b35ce7
ditaangelia70 <ditaangelia70@gmail.com>
2024-05-19 02:44:27 +0700

    all in

---
+++ resource/js/script.js
@@ -0,0 +1,9 @@
+document.getElementById('visiButton').addEventListener('click', function() {
+    document.getElementById('content').innerHTML = 
+    '<p>Menjadi Lembaga Sertifikasi Badan Usaha terdepan dalam meningkatkan kualitas usaha badan usaha jasa konstruksi yang lebih mandiri dan mempunyai daya saing tinggi</p>';
+});
+
+document.getElementById('misiButton').addEventListener('click', function() {
+    document.getElementById('content').innerHTML = 
+    '<p>1. Memberikan Pengakuan Kompetensi sumber daya kemampuan usaha dalam bidang jasa konstruksi di seluruh indonesia  <br>  2. Meningkatkan mutu sumber daya usaha melalui Lembaga sertifikasi badan usaha agar mampu meningkatkan kualitas pelayanan kepada masyarakat <br> 3. Menjadi lembaga sertifikasi badan usaha yang kredibel dan terpercaya agar sertifikat yang dikeluarkan diakui baik di taraf nasional maupun internasional <br> 4. Menyediakan tenaga professional yang memiliki kompetensi dan berorientasi terhadap kualitas pelayanan kepada masyarakat</p>';
+});
